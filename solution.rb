4f7dc2743c7f71126e7155c84eb5801c2ef31a02
github username <github email address>
2019-03-04 15:22:36 -0600

    done

--- solution.rb
+++ solution.rb
@@ -1,7 +1,43 @@
-class Deck
-
-end
-
+require 'pry'
 class Card
+    
+    attr_accessor :rank, :suit
+
+
+    def initialize(suit,rank)
+        @rank = rank
+        @suit = suit
+
+    end
+    
+end
+
+class Deck
+    
+    attr_accessor :rank, :suit, :cards
+    
+    def initialize 
+        @rank = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+        @suit = ["Hearts", "Clubs", "Diamonds", "Spades"]
+        @cards = []
+        rank.each do |rank|
+            suit.each do |suit|
+                @cards << Card.new(rank, suit)
+            end
+        end
+        
+    end
+
+    def self.cards
+        @cards
+    end
+
+    def choose_card
+        random_card = rand(cards.length)
+        @cards.delete_at(random_card)
+         
+    end
 
 end
+
+
