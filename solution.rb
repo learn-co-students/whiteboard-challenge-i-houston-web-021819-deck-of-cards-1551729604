2da18a93c5466d0bbcf842155f2df1141f4451a4
github username <github email address>
2019-03-04 15:25:39 -0600

    Done.

--- solution.rb
+++ solution.rb
@@ -1,7 +1,45 @@
+require 'pry'
 class Deck
+    attr_accessor :cards
+  def initialize
+    @ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    @suits = ["Hearts", "Clubs", "Dimonds", "Spades"]
+    @cards = []
 
+    @ranks.each do |rank|
+      @suits.each do |suit|
+        @cards << Card.new(suit, rank)
+      end   
+    end
+  end
+  
+
+  def choose_card
+    chosen_card = @cards.sample
+    @cards.delete(chosen_card)
+  end
 end
 
 class Card
-
+    attr_accessor :suit, :rank
+   def initialize(suit, rank)
+    @suit = suit
+    @rank = rank
+   end
 end
+
+
+
+
+
+=begin
+{:Hearts => ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"], 
+              :Clubs => ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"],
+              :Dimonds => ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"],
+              :Spades => ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]}
+
+@cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K",
+              "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K",
+              "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K",
+              "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+=end
